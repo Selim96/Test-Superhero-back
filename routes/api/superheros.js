a3da84e2ef17ae7372ce158b9380e3e9eaec45cf
Selim Fazylov <selim.fazylov1@gmail.com>
2022-08-04 23:15:03 +0300

    add isvalidate

--- routes/api/superheros.js
+++ routes/api/superheros.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { supers: cntrl } = require('../../controllers');
 const { ctrlWrapper } = require("../../helpers");
-const { validation } = require("../../middlewares");
+const { validation, isValidId } = require("../../middlewares");
 const { joiSchemas } = require("../../models");
 
 
@@ -9,12 +9,12 @@ const router = express.Router();
 
 router.get('/', ctrlWrapper(cntrl.getAll) );
 
-router.get('/:superId', ctrlWrapper(cntrl.getById));
+router.get('/:superId', isValidId, ctrlWrapper(cntrl.getById));
 
 router.post('/', validation(joiSchemas.createHero), ctrlWrapper(cntrl.create));
 
-router.patch('/:superId', validation(joiSchemas.editImage), ctrlWrapper(cntrl.editImage));
+router.patch('/:superId', isValidId, validation(joiSchemas.editImage), ctrlWrapper(cntrl.editImage));
 
-router.delete('/:superId', ctrlWrapper(cntrl.deleteById));
+router.delete('/:superId', isValidId, ctrlWrapper(cntrl.deleteById));
 
 module.exports = router;
