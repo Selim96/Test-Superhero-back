c3195645334cae0d98ceb0c4d99c899c8376ea71
Selim Fazylov <selim.fazylov1@gmail.com>
2022-08-05 16:30:27 +0300

    add morgan

--- controllers/supers/getById.js
+++ controllers/supers/getById.js
@@ -2,7 +2,7 @@ const { Superhero } = require("../../models");
 
 const getById = async (req, res) => {
     const { superId } = req.params;
-    const result = await Superhero.findById(superId, "-createdAt -updatedAt")
+    const result = await Superhero.findById(superId, "-createdAt -updatedAt");
     res.json(result)
 }
 
