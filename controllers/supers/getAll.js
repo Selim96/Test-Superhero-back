061591907270ea47d785fb95f6e77561e69856ea
Selim Fazylov <selim.fazylov1@gmail.com>
2022-08-06 13:37:43 +0300

    fix getall

--- controllers/supers/getAll.js
+++ controllers/supers/getAll.js
@@ -1,7 +1,7 @@
 const { Superhero } = require("../../models");
 
 const getAll = async (req, res) => {
-    const { page = 1, limit = 5 } = req.body;
+    const { page = 1, limit = 5 } = req.query;
     const skip = (page - 1) * limit;
     const result = await Superhero.find({}, "-createdAt -updatedAt", { skip, limit: Number(limit) }).populate("nickname");
     res.json(result);
