ca4b01543813cc39eb2c6abe99d6c275a231f417
Selim Fazylov <selim.fazylov1@gmail.com>
2022-08-07 20:01:08 +0300

    change get request add length

--- controllers/supers/getAll.js
+++ controllers/supers/getAll.js
@@ -4,7 +4,10 @@ const getAll = async (req, res) => {
     const { page = 1, limit = 5 } = req.query;
     const skip = (page - 1) * limit;
     const result = await Superhero.find({}, "-createdAt -updatedAt", { skip, limit: Number(limit) }).populate("nickname");
-    res.json(result);
+    
+    const data  = await Superhero.find({});
+    const dataLength = data.length;
+    res.json({result, dataLength});
 }
 
 module.exports = getAll;
